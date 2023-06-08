2c53b25c0ce50035bb58fd26c563f10d776ccdac
Geovanni Parra <parrageovanni04@gmail.com>
2023-06-08 16:36:36 -0400

    Initial Commit

---
+++ Loba 2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Loba 2
+//
+//  Created by Geovanni Parra on 6/8/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
